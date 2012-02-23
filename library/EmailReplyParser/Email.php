54d1b49bac90076a444ba71ccb545c95b5d4ea4b
Renan Gonçalves <renan.saddam@gmail.com>
2012-02-23 15:56:25 +0100

    Back-ported the code already, tests still missing.

---
+++ library/EmailReplyParser/Email.php
@@ -0,0 +1,170 @@
+<?php
+namespace EmailReplyParser;
+
+/**
+ * EmailReplyParser is a small library to parse plain text email content.  The
+ * goal is to identify which fragments are quoted, part of a signature, or
+ * original body content.  We want to support both top and bottom posters, so
+ * no simple "REPLY ABOVE HERE" content is used.
+ *
+ * Beyond RFC 5322 there aren't any real standards for how emails are created.
+ * This attempts to parse out common conventions for things like replies:
+ *
+ *     this is some text
+ *
+ *     On <date>, <author> wrote:
+ *     > blah blah
+ *     > blah blah
+ *
+ * ... and signatures:
+ *
+ *     this is some text
+ *
+ *     -- 
+ *     Bob
+ *     http://homepage.com/~bob
+ *
+ * Each of these are parsed into Fragment objects.
+ *
+ * EmailReplyParser also attempts to figure out which of these blocks should
+ * be hidden from users.
+ */
+class Email {
+
+/**
+ * List of Fragments this Email have.
+ *
+ * @param array
+ */
+	protected $_fragments = array();
+
+/**
+ * This determines if any 'visible' Fragment has been found.
+ * Once any visible Fragment is found, stop looking for hidden ones.
+ *
+ * @param boolean
+ */
+	protected $_foundVisible = false;
+
+/**
+ * This instance variable points to the current Fragment. If the matched
+ * line fits, it should be added to this Fragment. Otherwise, finish it
+ * and start a new Fragment.
+ *
+ * @param Fragment
+ */
+	protected $_fragment = null;
+
+/**
+ * Splits the given text into a list of Fragments.  This is roughly done by
+ * reversing the text and parsing from the bottom to the top.  This way we
+ * can check for 'On <date>, <author> wrote:' lines above quoted blocks.
+ *
+ * @param string $text A email body.
+ */
+	public function read($text) {
+		// The text is reversed initially due to the way we check for hidden fragments.
+		$text = Fragment::reverse($text);
+
+		// Split into lines.
+		$lines = preg_split("/\n/", $text);
+
+		// Scan each line of the email content.
+		foreach ($lines as $line) {
+			$this->_scanLine($line);
+		}
+
+		// Finish up the final fragment. Finishing a fragment will detect any
+		// attributes (hidden, signature, reply), and join each line into a string.
+		$this->_finishFragment();
+
+		// Now that parsing is done, reverse the order.
+		$this->_fragments = array_reverse($this->_fragments);
+		return $this->_fragments;
+	}
+
+/**
+ * Scans the given line of text and figures out which fragment it belongs to.
+ *
+ * @param string $line A line of text from the email
+ */
+	protected function _scanLine($line) {
+		$line = trim($line);
+
+		// We're looking for leading `>`'s to see if this line is part of a quoted Fragment.
+		$isQuoted = !!preg_match('/(>+)$/', $line);
+
+		// Mark the current Fragment as a signature if the current line is empty
+		// and the Fragment starts with a common signature indicator.
+		if ($this->_fragment && empty($line) && preg_match('/[\-\_]$/', $this->_fragment->getLastLine())) {
+			$this->_fragment->signature = true;
+			$this->_finishFragment();
+			return;
+		}
+
+		// If the line matches the current fragment, add it. Note that a common
+		// reply header also counts as part of the quoted Fragment, even though
+		// it doesn't start with `>`.
+		if ($this->_fragment &&
+			($this->_fragment->quoted === $isQuoted ||
+			($this->_fragment->quoted && ($this->_isQuotedHeader($line) || empty($line))))) {
+			$this->_fragment->lines[] = $line;
+		} else {
+			// Otherwise, finish the fragment and start a new one.
+			$this->_finishFragment();;
+			$this->_fragment = new Fragment($isQuoted, $line);
+		}
+	}
+
+/**
+ * Detects if a given line is a header above a quoted area.  It is only
+ * checked for lines preceding quoted regions.
+ * 
+ * @param string $line A line of text from the email.
+ */
+	protected function _isQuotedHeader($line) {
+		return !!preg_match('/^:etorw.*nO$/', $line);
+	}
+
+/**
+ * Builds the fragment string and reverses it, after all lines have been
+ * added.  It also checks to see if this Fragment is hidden.  The hidden
+ * Fragment check reads from the bottom to the top.
+ *
+ * Any quoted Fragments or signature Fragments are marked hidden if they
+ * are below any visible Fragments.  Visible Fragments are expected to
+ * contain original content by the author.  If they are below a quoted
+ * Fragment, then the Fragment should be visible to give context to the
+ * reply.
+ *
+ * Example:
+ *   some original text (visible)
+ *
+ *   > do you have any two's? (quoted, visible)
+ *
+ *   Go fish! (visible)
+ *
+ *   > -- 
+ *   > Player 1 (quoted, hidden)
+ *
+ *   -- 
+ *   Player 2 (signature, hidden)
+ */
+	protected function _finishFragment() {
+		if (!$this->_fragment) {
+			return;
+		}
+
+		$this->_fragment->finish();
+		if (!$this->_foundVisible) {
+			if ($this->_fragment->quoted || $this->_fragment->signature || empty($this->_fragment->content)) {
+				$this->_fragment->hidden = true;
+			} else {
+				$this->_foundVisible = true;
+			}
+		}
+
+		$this->_fragments[] = $this->_fragment;
+		$this->_fragment = null;
+	}
+}
---
+++ library/EmailReplyParser/Fragment.php
@@ -0,0 +1,84 @@
+<?php
+namespace EmailReplyParser;
+
+/**
+ * Represents a group of paragraphs in the email sharing common attributes.
+ * Paragraphs should get their own fragment if they are a quoted area or a
+ * signature.
+ */
+class Fragment {
+
+/**
+ * Lines in this Fragment.
+ *
+ * @param array
+ */
+	public $lines = array();
+
+/**
+ * Determines if this Fragment should be hidden from users.
+ *
+ * @param boolean
+ */
+	public $hidden = false;
+
+/**
+ * Determines if this Fragment is a signature.
+ *
+ * @param boolean
+ */
+	public $signature = false;
+
+/**
+ * Determines if this Fragment is a quote.
+ *
+ * @param boolean
+ */
+	public $quoted = false;
+
+/**
+ * This is reserved for the joined String that is build when this Fragment is finished.
+ *
+ * @param string
+ */
+	public $content;
+
+/**
+ * Store the first line and marks the Fragment as quoted, if it is.
+ *
+ * @param boolean $isQuoted Eitheir if the line if quoted or not.
+ * @param string $firstLine A line of text from the email.
+ */
+	public function __construct($isQuoted, $firstLine) {
+		$this->quoted = $isQuoted;
+		$this->lines[] = $firstLine;
+	}
+
+/**
+ * Builds the string content by joining the lines and reversing them.
+ *
+ */
+	public function finish() {
+		$this->content = implode("\n", $this->lines);
+		$this->content = Fragment::reverse($this->content);
+	}
+
+/**
+ * Get the last line of this Fragment.
+ *
+ */
+	public function getLastLine() {
+		$count = count($this->lines);
+		return $this->lines[$count - 1];
+	}
+
+/**
+ * Utility method to reverse a text string.
+ *
+ * @param string $text
+ */
+	public static function reverse($text) {
+		preg_match_all('/./us', $text, $matches);
+		return join('', array_reverse($matches[0]));
+	}
+}
